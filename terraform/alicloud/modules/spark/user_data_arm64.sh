3cd021f9d7b73922b75d0687556035fbc0487bf0
Yi Chen <github@chenyicn.net>
2025-01-08 16:20:46 +0800

    Add docs about benchmark on ACK with arm64 arch (#8)
    Signed-off-by: Yi Chen <github@chenyicn.net>

---
+++ terraform/alicloud/modules/spark/user_data_arm64.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# 打印命令
+set -ex
+
+# 添加 parted
+yum install -y parted e2fsprogs
+
+# 为数据盘新建分区
+disks=(/dev/nvme1n1 /dev/nvme2n1 /dev/nvme3n1 /dev/nvme4n1 /dev/nvme5n1 /dev/nvme6n1)
+for disk in ${disks[@]}; do
+    parted ${disk} mklabel gpt
+    parted ${disk} mkpart primary 1 100%
+    parted ${disk} align-check optimal 1
+done
+partprobe
+
+# 为分区创建文件系统
+for disk in ${disks[@]}; do
+    mkfs -t xfs ${disk}p1
+done
+
+# 挂载分区
+cp /etc/fstab /etc/fstab.bak
+n=${#disks[@]}
+for ((i = 0; i < n; i++)); do
+    dir="/mnt/disk$(($i + 1))"
+    mkdir -p ${dir}
+    echo "$(blkid ${disks[i]}p1 | awk '{print $2}' | sed 's/\"//g') ${dir} xfs defaults 0 0" >>/etc/fstab
+done
+mount -a
+
+# 修改挂载点访问权限
+chmod a+w /mnt/disk*
