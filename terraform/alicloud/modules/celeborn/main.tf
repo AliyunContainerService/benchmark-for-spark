d8b26c8a717c3bdcdaf5c4147c10d0ba319cf778
Yi Chen <github@chenyicn.net>
2025-01-08 21:02:30 +0800

    terraform: update celeborn master/worker node pool and user data (#9)
    Signed-off-by: Yi Chen <github@chenyicn.net>

--- terraform/alicloud/modules/celeborn/main.tf
+++ terraform/alicloud/modules/celeborn/main.tf
@@ -1,20 +1,68 @@
-resource "alicloud_cs_kubernetes_node_pool" "celeborn" {
-  node_pool_name                = "celeborn"
+resource "alicloud_cs_kubernetes_node_pool" "celeborn-master" {
+  node_pool_name                = "celeborn-master"
   cluster_id                    = var.cluster_id
   vswitch_ids                   = var.vswitch_ids
-  desired_size                  = var.instance_count
-  instance_types                = [var.instance_type]
+  desired_size                  = var.master_instance_count
+  instance_types                = [var.master_instance_type]
+  image_type                    = "AliyunLinux3"
+  system_disk_category          = "cloud_essd"
+  system_disk_size              = 40
+  system_disk_performance_level = "PL1"
+
+  data_disks {
+    category          = "cloud_essd"
+    size              = 300
+    performance_level = "PL1"
+    device            = "/dev/vdb"
+  }
+
+  data_disks {
+    category          = "cloud_essd"
+    size              = 40
+    performance_level = "PL1"
+    device            = "/dev/vdc"
+  }
+
+  labels {
+    key   = "celeborn.apache.org/role"
+    value = "master"
+  }
+
+  taints {
+    key    = "celeborn.apache.org/role"
+    value  = "master"
+    effect = "NoSchedule"
+  }
+
+  user_data = base64encode(file("${path.module}/master_user_data.sh"))
+
+  resource_group_id  = var.resource_group_id
+  security_group_ids = [var.security_group_id]
+}
+
+resource "alicloud_cs_kubernetes_node_pool" "celeborn-worker" {
+  node_pool_name                = "celeborn-worker"
+  cluster_id                    = var.cluster_id
+  vswitch_ids                   = var.vswitch_ids
+  desired_size                  = var.worker_instance_count
+  instance_types                = [var.worker_instance_type]
   image_type                    = "AliyunLinux3"
   system_disk_category          = "cloud_essd"
   system_disk_size              = 40
   system_disk_performance_level = "PL1"
 
   labels {
-    key   = "tpcds.benchmark.role"
-    value = "celeborn"
+    key   = "celeborn.apache.org/role"
+    value = "worker"
   }
 
-  user_data = base64encode(file("${path.module}/user_data.sh"))
+  taints {
+    key    = "celeborn.apache.org/role"
+    value  = "worker"
+    effect = "NoSchedule"
+  }
+
+  user_data = base64encode(file("${path.module}/worker_user_data.sh"))
 
   resource_group_id  = var.resource_group_id
   security_group_ids = [var.security_group_id]
--- terraform/alicloud/terraform.tfvars
+++ terraform/alicloud/terraform.tfvars
@@ -8,10 +8,12 @@ spark_master_instance_type  = "ecs.g7.2xlarge"
 spark_worker_instance_count = 6
 spark_worker_instance_type  = "ecs.g7.8xlarge"
 
+# Celeborn master node pool
+celeborn_master_instance_count = 0
+celeborn_master_instance_type  = "ecs.g8i.2xlarge"
+celeborn_worker_instance_count = 0
+celeborn_worker_instance_type  = "ecs.i4.8xlarge"
+
 # Fluid node pool
 fluid_instance_count = 0
 fluid_instance_type  = "ecs.i3.2xlarge"
-
-# Celeborn node pool
-celeborn_instance_count = 0
-celeborn_instance_type  = "ecs.i3.2xlarge"
